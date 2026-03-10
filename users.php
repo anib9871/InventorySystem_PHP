30f82121dc68aaa5939c750d2ee5a0b31b8130b2
anib9871 <anib9871@gmail.com>
2026-03-10 18:33:30 +0530

    Update users.php

--- users.php
+++ users.php
@@ -7,25 +7,53 @@
  page_require_level(1);
 //pull out all user form database
  $all_users = find_all_user();
+ $edit_user = null;
+
+if(isset($_GET['edit'])){
+  $edit_user = find_by_id('users',(int)$_GET['edit']);
+}
 ?>
 <?php include_once('layouts/header.php'); ?>
+
 <div class="row">
    <div class="col-md-12">
      <?php echo display_msg($msg); ?>
    </div>
 </div>
+
 <div class="row">
-  <div class="col-md-12">
-    <div class="panel panel-default">
-      <div class="panel-heading clearfix">
-        <strong>
-          <span class="glyphicon glyphicon-th"></span>
-          <span>Users</span>
-       </strong>
-         <a href="add_user.php" class="btn btn-info pull-right">Add New User</a>
-      </div>
-     <div class="panel-body">
-      <table class="table table-bordered table-striped">
+
+<!-- LEFT SIDE : CREATE USER -->
+<div class="col-md-4">
+<?php include_once('add_user.php'); ?>
+</div>
+
+<!-- RIGHT SIDE : USERS LIST -->
+<div class="col-md-8">
+
+<div class="panel panel-default">
+
+<div class="panel-heading">
+<strong>
+<span class="glyphicon glyphicon-th"></span>
+Users List
+</strong>
+</div>
+
+</div>
+
+
+<div class="panel-body">
+
+<!-- SEARCH BAR -->
+<div class="row" style="margin-bottom:10px;">
+  <div class="col-md-4">
+    <input type="text" id="userSearch" class="form-control" placeholder="Search user...">
+  </div>
+</div>
+
+<div class="table-responsive">
+<table class="table table-bordered table-striped" id="usersTable">
         <thead>
           <tr>
             <th class="text-center" style="width: 50px;">#</th>
@@ -54,7 +82,7 @@
            <td><?php echo read_date($a_user['last_login'])?></td>
            <td class="text-center">
              <div class="btn-group">
-                <a href="edit_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit">
+                <a href="users.php?edit=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit">
                   <i class="glyphicon glyphicon-pencil"></i>
                </a>
                 <a href="delete_user.php?id=<?php echo (int)$a_user['id'];?>" class="btn btn-xs btn-danger" data-toggle="tooltip" title="Remove">
@@ -64,10 +92,34 @@
            </td>
           </tr>
         <?php endforeach;?>
-       </tbody>
-     </table>
-     </div>
-    </div>
-  </div>
+</tbody>
+</table>
 </div>
-  <?php include_once('layouts/footer.php'); ?>
+</div>
+</div>
+
+
+<?php include_once('layouts/footer.php'); ?>
+
+<script>
+
+document.getElementById("userSearch").addEventListener("keyup", function() {
+
+let filter = this.value.toLowerCase();
+let rows = document.querySelectorAll("#usersTable tbody tr");
+
+rows.forEach(function(row){
+
+let text = row.textContent.toLowerCase();
+
+if(text.indexOf(filter) > -1){
+row.style.display = "";
+}else{
+row.style.display = "none";
+}
+
+});
+
+});
+
+</script>
