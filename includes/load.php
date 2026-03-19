b7acf3af1760f9bc041c916a3013ef7e8a403ff3
anib9871 <anib9871@gmail.com>
2026-03-19 17:10:14 +0530

    Update load.php

--- includes/load.php
+++ includes/load.php
@@ -1,12 +1,12 @@
-<?php
-date_default_timezone_set('Asia/Kolkata');
-ob_start();
-session_start();
 
+<?php
+
+date_default_timezone_set('Asia/Kolkata');
 // -----------------------------------------------------------------------
 // DEFINE SEPERATOR ALIASES
 // -----------------------------------------------------------------------
 define("URL_SEPARATOR", '/');
+
 define("DS", DIRECTORY_SEPARATOR);
 
 // -----------------------------------------------------------------------
@@ -15,9 +15,12 @@ define("DS", DIRECTORY_SEPARATOR);
 defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
 define("LIB_PATH_INC", SITE_ROOT.DS);
 
+
 require_once(LIB_PATH_INC.'config.php');
 require_once(LIB_PATH_INC.'functions.php');
 require_once(LIB_PATH_INC.'session.php');
 require_once(LIB_PATH_INC.'upload.php');
 require_once(LIB_PATH_INC.'database.php');
 require_once(LIB_PATH_INC.'sql.php');
+
+?>
