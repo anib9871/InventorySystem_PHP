6d6c4a0bd72d42c13566ff960371b387554ea245
anib9871 <anib9871@gmail.com>
2026-03-19 17:25:11 +0530

    Add files via upload

---
+++ layouts/superadmin_menu.php
@@ -0,0 +1,49 @@
+<ul>
+
+<li>
+<a href="superadmin_dashboard.php">
+<i class="glyphicon glyphicon-home"></i>
+<span>Dashboard</span>
+</a>
+</li>
+
+<!-- ===== MASTERS ===== -->
+
+<li>
+<a href="#" class="submenu-toggle">
+<i class="glyphicon glyphicon-briefcase"></i>
+<span>Masters</span>
+</a>
+
+<ul class="nav submenu">
+
+<li>
+<a href="master_organization.php">
+Organization
+</a>
+</li>
+
+<li>
+<a href="master_center.php">
+Centers
+</a>
+</li>
+
+<li>
+<a href="master_role.php">
+Role
+</a>
+</li>
+
+
+<li>
+<a href="user_credentials.php">
+User Credentials
+</a>
+</li>
+
+</ul>
+
+</li>
+
+</ul>
