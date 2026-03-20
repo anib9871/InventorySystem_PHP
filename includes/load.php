eeaf9b59ab74060faef418dfefb048eb899dd619
anib9871 <anib9871@gmail.com>
2026-03-20 13:14:29 +0530

    Update load.php

--- includes/load.php
+++ includes/load.php
@@ -1,20 +1,13 @@
-
 <?php
+ob_start();
 
 date_default_timezone_set('Asia/Kolkata');
-// -----------------------------------------------------------------------
-// DEFINE SEPERATOR ALIASES
-// -----------------------------------------------------------------------
-define("URL_SEPARATOR", '/');
 
+define("URL_SEPARATOR", '/');
 define("DS", DIRECTORY_SEPARATOR);
 
-// -----------------------------------------------------------------------
-// DEFINE ROOT PATHS
-// -----------------------------------------------------------------------
 defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
 define("LIB_PATH_INC", SITE_ROOT.DS);
-
 
 require_once(LIB_PATH_INC.'config.php');
 require_once(LIB_PATH_INC.'functions.php');
