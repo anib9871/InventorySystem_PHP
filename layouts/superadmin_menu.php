545428748edbd5e3d8ef24e0acb64fca0eaaae94
anib9871 <anib9871@gmail.com>
2026-05-15 14:50:30 +0530

    Update superadmin_menu.php

--- layouts/superadmin_menu.php
+++ layouts/superadmin_menu.php
@@ -1,54 +1,52 @@
-<ul>
+<ul id="sidebarMenu">
 
+<!-- DASHBOARD -->
 <li>
-<a href="superadmin_dashboard.php">
-<i class="glyphicon glyphicon-home"></i>
-<span>Dashboard</span>
-</a>
+  <a href="superadmin_dashboard.php">
+    <i class="glyphicon glyphicon-home"></i>
+    <span>Dashboard</span>
+  </a>
 </li>
 
 <!-- ===== MASTERS ===== -->
-
 <li>
-<a href="#" class="submenu-toggle">
-<i class="glyphicon glyphicon-briefcase"></i>
-<span>Masters</span>
-</a>
+  <a href="#" class="submenu-toggle">
+    <i class="glyphicon glyphicon-briefcase"></i>
+    <span>Masters</span>
+    <span class="arrow"><i class="glyphicon glyphicon-chevron-right"></i></span>
+  </a>
 
-<ul class="nav submenu">
+  <ul class="submenu">
 
-<li>
-<a href="master_organization.php">
-Organization
-</a>
-</li>
+    <li>
+      <a href="master_organization.php">
+        <i class="glyphicon glyphicon-th-large"></i>
+        Organization
+      </a>
+    </li>
 
-<li>
-<a href="master_center.php">
-Centers
-</a>
-</li>
+    <li>
+      <a href="master_role.php">
+        <i class="glyphicon glyphicon-user"></i>
+        Role
+      </a>
+    </li>
 
-<li>
-<a href="master_role.php">
-Role
-</a>
-</li>
+    <li>
+      <a href="user_credentials.php">
+        <i class="glyphicon glyphicon-lock"></i>
+        User Credentials
+      </a>
+    </li>
 
+    <li>
+      <a href="subscription_plans.php">
+        <i class="glyphicon glyphicon-list-alt"></i>
+        Subscription Plans
+      </a>
+    </li>
 
-<li>
-<a href="user_credentials.php">
-User Credentials
-</a>
-</li>
-
-<li>
-  <a href="subscription_plans.php">Subscription Plans</a>
-</li>
-
-
-</ul>
-
+  </ul>
 </li>
 
 </ul>
