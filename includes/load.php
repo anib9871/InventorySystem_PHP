fdc1c6473ff0f48a56a0a2c274080852f409d605
anib9871 <anib9871@gmail.com>
2026-05-15 11:25:58 +0530

    Add GST and expiry configuration loading

--- includes/load.php
+++ includes/load.php
@@ -15,3 +15,63 @@ require_once(LIB_PATH_INC.'session.php');
 require_once(LIB_PATH_INC.'upload.php');
 require_once(LIB_PATH_INC.'database.php');
 require_once(LIB_PATH_INC.'sql.php');
+/* ================= GST CONFIG ================= */
+
+$gst_enabled = "Yes";
+
+if(isset($_SESSION['org_id'])){
+
+    $org_id = (int)$_SESSION['org_id'];
+
+    $cfg = find_by_sql("
+        SELECT gst_registered
+        FROM configuration_master
+        WHERE org_id = '{$org_id}'
+        LIMIT 1
+    ");
+
+    if($cfg){
+        $gst_enabled = $cfg[0]['gst_registered'];
+    }
+}
+
+/* ================= EXPIRY + PRINT CONFIG ================= */
+
+$expiry_required = "Yes";
+
+$print_name = "A4";
+$print_css_width = "190mm";
+
+if(isset($_SESSION['org_id'])){
+
+    $org_id = (int)$_SESSION['org_id'];
+
+    $cfg2 = find_by_sql("
+
+        SELECT c.expiry_required,
+               p.print_name,
+               p.css_width
+
+        FROM configuration_master c
+
+        LEFT JOIN print_type_master p
+        ON p.id = c.print_type_id
+
+        WHERE c.org_id = '{$org_id}'
+        LIMIT 1
+    ");
+
+    if($cfg2){
+
+        $expiry_required =
+            $cfg2[0]['expiry_required'];
+
+        $print_name =
+            $cfg2[0]['print_name'];
+
+        $print_css_width =
+            $cfg2[0]['css_width'];
+    }
+}
+
+?>
