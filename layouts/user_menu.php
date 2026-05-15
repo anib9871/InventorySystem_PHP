8fa6ca28c202eb5946d2a9602de28959cf049b14
anib9871 <anib9871@gmail.com>
2026-05-15 11:33:36 +0530

    Refactor user menu with inventory and billing panels

--- layouts/user_menu.php
+++ layouts/user_menu.php
@@ -1,29 +1,178 @@
-<ul>
-  <li>
-    <a href="home.php">
-      <i class="glyphicon glyphicon-home"></i>
-      <span>Dashboard</span>
-    </a>
-  </li>
-  <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-th-list"></i>
-       <span>Sales</span>
+<ul id="sidebarMenu">
+
+<?php 
+
+$combined = (isset($_SESSION['combined_mode']) && $_SESSION['combined_mode'] == 1);
+
+$inventoryOnly = (
+isset($_SESSION['inventory_access']) &&
+$_SESSION['inventory_access'] == 1 &&
+!$combined
+);
+
+$billingOnly = (
+isset($_SESSION['billing_access']) &&
+$_SESSION['billing_access'] == 1 &&
+!$combined
+);
+
+$showInventory = $combined || $inventoryOnly;
+$showBilling   = $combined || $billingOnly;
+
+?>
+
+<!-- ================= INVENTORY USER ================= -->
+
+<?php if($showInventory): ?>
+
+<li class="inventory-header">
+
+  <a href="#" class="submenu-toggle">
+
+    <i class="glyphicon glyphicon-folder-open"></i>
+
+    INVENTORY USER PANEL
+
+    <span class="arrow">
+      <i class="glyphicon glyphicon-chevron-right"></i>
+    </span>
+
+  </a>
+
+  <ul class="submenu">
+
+    <li>
+      <a href="user_dashboard.php">
+        <i class="glyphicon glyphicon-home"></i>
+        Dashboard
       </a>
-      <ul class="nav submenu">
-         <li><a href="sales.php">Manage Sales</a> </li>
-         <li><a href="add_sale.php">Add Sale</a> </li>
-     </ul>
-  </li>
-  <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-signal"></i>
-       <span>Sales Report</span>
+    </li>
+
+    <li>
+
+      <a href="#" class="submenu-toggle">
+
+        <i class="glyphicon glyphicon-credit-card"></i>
+
+        Transaction
+
+        <span class="arrow">
+          <i class="glyphicon glyphicon-chevron-right"></i>
+        </span>
+
       </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
+
+      <ul class="submenu">
+
+        <li>
+          <a href="grn.php">
+            GRN
+          </a>
+        </li>
+
+        <li>
+          <a href="invoice_create.php">
+            Invoice
+          </a>
+        </li>
+
+        <li>
+          <a href="invoice_list.php">
+            Save Invoice
+          </a>
+        </li>
+
+        
+
       </ul>
-  </li>
+
+    </li>
+
+  </ul>
+
+</li>
+
+<?php endif; ?>
+
+
+<!-- ================= BILLING USER ================= -->
+
+<?php if($showBilling): ?>
+
+<li class="billing-header">
+
+  <a href="#" class="submenu-toggle">
+
+    <i class="glyphicon glyphicon-user"></i>
+
+    BILLING USER PANEL
+
+    <span class="arrow">
+      <i class="glyphicon glyphicon-chevron-right"></i>
+    </span>
+
+  </a>
+
+  <ul class="submenu">
+
+    <li>
+      <a href="user_dashboard.php">
+
+        <i class="glyphicon glyphicon-home"></i>
+
+        Dashboard
+
+      </a>
+    </li>
+
+    <li>
+
+      <a href="#" class="submenu-toggle">
+
+        <i class="glyphicon glyphicon-credit-card"></i>
+
+        Transaction
+
+        <span class="arrow">
+          <i class="glyphicon glyphicon-chevron-right"></i>
+        </span>
+
+      </a>
+
+      <ul class="submenu">
+
+        <li>
+          <a href="invoice_create.php">
+            Direct Billing
+          </a>
+        </li>
+
+        <li>
+          <a href="invoice_list.php">
+            Save Invoice
+          </a>
+        </li>
+        <!-- <li><a href="billing_sales.php">Sales Report</a></li> -->
+      </ul>
+
+    </li>
+   <!-- REPORT -->
+    <li>
+      <a href="#" class="submenu-toggle" data-menu="billing_reports">
+        <i class="glyphicon glyphicon-credit-card"></i>
+        Report
+        <span class="arrow"><i class="glyphicon glyphicon-chevron-right"></i></span>
+      </a>
+
+      <ul class="submenu">
+        <li><a href="business_report.php">Sales Report</a></li>
+        <!-- <li><a href="billing_sales.php">Report</a></li> -->
+  </ul>
+</li>
+  </ul>
+
+</li>
+
+<?php endif; ?>
+
 </ul>
