db22bd29f89df066f62306ba64368b407865298a
anib9871 <anib9871@gmail.com>
2026-05-16 17:01:07 +0530

    Refactor edit_account.php for sequence updates

--- edit_account.php
+++ edit_account.php
@@ -1,105 +1,118 @@
 <?php
-  $page_title = 'Edit Account';
-  require_once('includes/load.php');
-//   page_require_level(3);
-?>
-<?php
-//update user image
-  if(isset($_POST['submit'])) {
-  $photo = new Media();
-  $user_id = (int)$_POST['user_id'];
-  $photo->upload($_FILES['file_upload']);
-  if($photo->process_user($user_id)){
-    $session->msg('s','photo has been uploaded.');
-    redirect('edit_account.php');
-    } else{
-      $session->msg('d',join($photo->errors));
-      redirect('edit_account.php');
-    }
+require_once('includes/load.php');
+//page_require_level(2);
+
+$id = (int)$_GET['id'];
+
+$sequence = find_by_sql("SELECT * FROM sequence_master WHERE sequence_id = $id LIMIT 1");
+
+
+if(!$sequence){
+  $session->msg('d',"Sequence not found");
+  redirect('master_sequence.php');
+}
+$sequence = $sequence[0];
+/* UPDATE */
+if(isset($_POST['update_sequence'])){
+
+  $category = remove_junk($_POST['sequence_category']);
+  $last_no  = (int)$_POST['last_no'];
+  $prefix = remove_junk($_POST['prefix']);
+
+
+$sql = "UPDATE sequence_master SET
+
+sequence_category = '{$category}',
+last_no = '{$last_no}',
+prefix = '{$prefix}'
+
+WHERE sequence_id = '{$id}'";
+
+  if($db->query($sql)){
+      $session->msg('s',"Sequence Updated");
+      redirect('master_sequence.php',false);
+  }else{
+      $session->msg('d',"Update Failed");
+      redirect('master_sequence.php',false);
   }
+
+}
+
 ?>
-<?php
- //update user other info
-  if(isset($_POST['update'])){
-    $req_fields = array('name','username' );
-    validate_fields($req_fields);
-    if(empty($errors)){
-             $id = (int)$_SESSION['user_id'];
-           $name = remove_junk($db->escape($_POST['name']));
-       $username = remove_junk($db->escape($_POST['username']));
-            $sql = "UPDATE users SET name ='{$name}', username ='{$username}' WHERE id='{$id}'";
-    $result = $db->query($sql);
-          if($result && $db->affected_rows() === 1){
-            $session->msg('s',"Acount updated ");
-            redirect('edit_account.php', false);
-          } else {
-            $session->msg('d',' Sorry failed to updated!');
-            redirect('edit_account.php', false);
-          }
-    } else {
-      $session->msg("d", $errors);
-      redirect('edit_account.php',false);
-    }
-  }
-?>
+
 <?php include_once('layouts/header.php'); ?>
+
 <div class="row">
-  <div class="col-md-12">
-    <?php echo display_msg($msg); ?>
-  </div>
-  <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <div class="panel-heading clearfix">
-            <span class="glyphicon glyphicon-camera"></span>
-            <span>Change My photo</span>
-          </div>
-        </div>
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-md-4">
-                <img class="img-circle img-size-2" src="uploads/users/<?php echo $user['image'];?>" alt="">
-            </div>
-            <div class="col-md-8">
-              <form class="form" action="edit_account.php" method="POST" enctype="multipart/form-data">
-              <div class="form-group">
-                <input type="file" name="file_upload" multiple="multiple" class="btn btn-default btn-file"/>
-              </div>
-              <div class="form-group">
-                <input type="hidden" name="user_id" value="<?php echo $user['id'];?>">
-                 <button type="submit" name="submit" class="btn btn-warning">Change</button>
-              </div>
-             </form>
-            </div>
-          </div>
-        </div>
-      </div>
-  </div>
-  <div class="col-md-6">
-    <div class="panel panel-default">
-      <div class="panel-heading clearfix">
-        <span class="glyphicon glyphicon-edit"></span>
-        <span>Edit My Account</span>
-      </div>
-      <div class="panel-body">
-          <form method="post" action="edit_account.php?id=<?php echo (int)$user['id'];?>" class="clearfix">
-            <div class="form-group">
-                  <label for="name" class="control-label">Name</label>
-                  <input type="name" class="form-control" name="name" value="<?php echo remove_junk(ucwords($user['name'])); ?>">
-            </div>
-            <div class="form-group">
-                  <label for="username" class="control-label">Username</label>
-                  <input type="text" class="form-control" name="username" value="<?php echo remove_junk(ucwords($user['username'])); ?>">
-            </div>
-            <div class="form-group clearfix">
-                    <a href="change_password.php" title="change password" class="btn btn-danger pull-right">Change Password</a>
-                    <button type="submit" name="update" class="btn btn-info">Update</button>
-            </div>
-        </form>
-      </div>
-    </div>
-  </div>
+<div class="col-md-6">
+
+<div class="panel panel-default">
+
+<div class="panel-heading">
+<strong>Edit Sequence</strong>
 </div>
 
+<div class="panel-body">
+
+<form method="post">
+
+<div class="form-group">
+<label>Sequence Category</label>
+<select name="sequence_category" class="form-control" required>
+
+<option value="invoice"
+<?php if($sequence['sequence_category']=='invoice') echo 'selected'; ?>>
+Invoice
+</option>
+
+<option value="quotation"
+<?php if($sequence['sequence_category']=='quotation') echo 'selected'; ?>>
+Quotation
+</option>
+
+</select>
+</div>
+
+<div class="form-group">
+<label>Last No</label>
+<input type="number"
+name="last_no"
+class="form-control"
+value="<?= $sequence['last_no']; ?>"
+required>
+</div>
+
+<div class="form-group">
+<label>Prefix</label>
+
+<input type="text"
+name="prefix"
+class="form-control"
+value="<?= $sequence['prefix']; ?>">
+</div>
+
+<!-- <div class="form-group">
+<label>Total Digits</label>
+
+<input type="number"
+name="total_digits"
+class="form-control"
+value="<?= $sequence['total_digits']; ?>">
+</div> -->
+
+<button class="btn btn-success" name="update_sequence">
+Update
+</button>
+
+<a href="master_sequence.php" class="btn btn-default">
+Cancel
+</a>
+
+</form>
+
+</div>
+</div>
+
+</div>
+</div>
 
 <?php include_once('layouts/footer.php'); ?>
