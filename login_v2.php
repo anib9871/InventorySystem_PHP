51eaf5da1290fe2874fd03584b33b8c70daccb9c
anib9871 <anib9871@gmail.com>
2026-05-19 13:19:16 +0530

    Update login_v2.php

--- login_v2.php
+++ login_v2.php
@@ -1,27 +1,59 @@
 <?php
-  ob_start();
-  require_once('includes/load.php');
-  if($session->isUserLoggedIn(true)) { redirect('home.php', false); }
+require_once('includes/load.php');
+
+/* SUPERADMIN ALREADY LOGIN */
+
+if(isset($_SESSION['superadmin_login'])){
+header("Location: superadmin_dashboard.php");
+exit();
+}
+
+/* NORMAL USER ALREADY LOGIN */
+
+if($session->isUserLoggedIn(true)){
+redirect('home.php', false);
+}
 ?>
+
 <?php include_once('layouts/header.php'); ?>
+
 <div class="login-page">
     <div class="text-center">
        <h1>Welcome</h1>
        <p>Sign in to start your session</p>
     </div>
+
     <?php echo display_msg($msg); ?>
-    <form method="post" action="auth_v2.php" class="clearfix">
+
+    <form method="post" action="auth_v2.php">
+
         <div class="form-group">
             <label>Username</label>
-            <input type="text" class="form-control" name="username" placeholder="Username">
+            <input type="text" class="form-control" name="username" required>
         </div>
+
         <div class="form-group">
             <label>Password</label>
-            <input type="password" name="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" name="password" required>
         </div>
+
         <div class="form-group">
-            <button type="submit" class="btn btn-danger">Login</button>
+            <button type="submit" class="btn btn-info pull-right">Login</button>
         </div>
+
     </form>
+
+    <hr>
+
+<div class="text-center">
+
+<a href="user_login.php" class="btn btn-default">
+
+Employee / Staff Login
+
+</a>
+
 </div>
+</div>
+
 <?php include_once('layouts/footer.php'); ?>
