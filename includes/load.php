a946448f16002dfba17c480a472ae3553486fec9
anib9871 <anib9871@gmail.com>
2026-05-21 14:17:04 +0530

    Update session handling and redirect headers

--- includes/load.php
+++ includes/load.php
@@ -27,8 +27,12 @@ if(isset($_SESSION['app_version'])){
         session_unset();
         session_destroy();
 
-        header("Location: index.php");
-        exit();
+       header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+header("Location: index.php?reload=".time());
+exit();
     }
 
 }
