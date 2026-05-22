8443dd68f5990c68e3ddefe1791453d4877a2724
anib9871 <anib9871@gmail.com>
2026-05-22 10:49:09 +0530

    Refactor login and logout methods in session.php
    Refactor session management methods to streamline login and logout processes.

--- includes/session.php
+++ includes/session.php
@@ -19,69 +19,39 @@ class Session {
     return $this->user_is_logged_in;
  }
 
- public function login($user_id){
+public function login($user_id){
 
     session_regenerate_id(true);
 
     $_SESSION['user_id'] = $user_id;
- }
+}
 
  private function userLoginSetup(){
-
-    $this->user_is_logged_in =
-        isset($_SESSION['user_id']) &&
-        isset($_SESSION['username']);
+    $this->user_is_logged_in = isset($_SESSION['user_id']);
  }
 
  public function logout(){
-
-    $_SESSION = array();
-
-    if (ini_get("session.use_cookies")) {
-
-      $params = session_get_cookie_params();
-
-      setcookie(
-         session_name(),
-         '',
-         time() - 42000,
-         $params["path"],
-         $params["domain"],
-         $params["secure"],
-         $params["httponly"]
-      );
-    }
-
-    session_destroy();
+    unset($_SESSION['user_id']);
  }
 
  public function msg($type ='', $msg =''){
-
     if(!empty($msg)){
-
        if(strlen(trim($type)) == 1){
-
          $type = str_replace(
            array('d','i','w','s'),
            array('danger','info','warning','success'),
            $type
          );
        }
-
        $_SESSION['msg'][$type] = $msg;
-
     } else {
-
       return $this->msg;
     }
  }
 
  private function flash_msg(){
-
     if(isset($_SESSION['msg'])){
-
       $this->msg = $_SESSION['msg'];
-
       unset($_SESSION['msg']);
     }
  }
