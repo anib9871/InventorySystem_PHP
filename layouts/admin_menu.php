3d96d1106ceec0da05cb4be6d1e424d7f6c6c77a
anib9871 <anib9871@gmail.com>
2026-06-03 19:23:05 +0530

    Add Supplier link to admin menu

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -94,6 +94,7 @@ if(!empty($config)){
         <li><a href="manufacture.php">Manufacture</a></li>
          <li><a href="payments.php">Manage Payments</a></li>
         <li><a href="payment_report.php">Payments Report</a></li>
+        <li><a href="supplier_advance.php">Supplier</a></li>
 
       </ul>
     </li>
