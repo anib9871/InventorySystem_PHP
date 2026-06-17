ca02e055d2b1cf8b4afe63e693d0e38ee1105c64
anib9871 <anib9871@gmail.com>
2026-06-17 17:33:53 +0530

    Update footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,8 +2,8 @@
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
-  <script type="text/javascript" src="/libs/js/functions.js?v=<?php echo time(); ?>"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
+  <script type="text/javascript" src="libs/js/functions.js"></script>
 
   <!-- ✅ SIDEBAR TOGGLE SCRIPT -->
 <script>
@@ -62,8 +62,46 @@ $(document).ready(function(){
 
 });
 </script>
+
+<script>
+$(document).ready(function(){
+
+    $('#bill_date').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true,
+        todayHighlight: true
+    });
+
+});
+</script>
+
+<script>
+
+function deleteGRN(bill){
+
+    Swal.fire({
+        title: 'Delete GRN?',
+        text: 'This action cannot be undone.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, Delete'
+    }).then((result) => {
+
+        if(result.isConfirmed){
+
+            window.location =
+            'delete_grn.php?bill=' + encodeURIComponent(bill);
+
+        }
+
+    });
+
+}
+
+</script>
 </body>
-</html> 
+</html>
 
 <?php if(isset($db)) { $db->db_disconnect(); } ?>
-
