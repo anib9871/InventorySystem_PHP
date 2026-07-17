49abda41065067d428be8839aa7f15217bfa64ff
anib9871 <anib9871@gmail.com>
2026-07-17 17:59:46 +0530

    Update organization_master.php

--- organization_master.php
+++ organization_master.php
@@ -124,8 +124,169 @@ include_once('layouts/header.php');
 ?>
 
 <style>
-.add-box{ margin-top:-50px; }
-.equal-btn{ min-width:60px; text-align:center; }
+.add-box{
+    margin-top:0;
+}
+
+.panel{
+    border-radius:10px;
+    margin-bottom:12px;
+}
+
+.panel-body{
+    padding:12px 15px;
+}
+
+.panel-heading{
+    padding:10px 15px;
+    font-size:14px;
+    font-weight:600;
+}
+
+label{
+    font-size:12px;
+    margin-bottom:4px;
+    font-weight:600;
+}
+
+.form-control{
+    height:32px;
+    font-size:12px;
+    padding:4px 8px;
+}
+
+textarea.form-control{
+    height:55px;
+    resize:none;
+    padding-top:6px;
+}
+
+.btn{
+    height:32px;
+    font-size:12px;
+    padding:4px 12px;
+}
+
+.row{
+    margin-bottom:8px;
+}
+
+.table{
+    margin-bottom:0;
+    font-size:12px;
+}
+
+.table thead{
+    background:#18233b;
+    color:#fff;
+}
+
+.table thead th{
+    border:none !important;
+    padding:11px 10px;
+    white-space:nowrap;
+    vertical-align:middle !important;
+}
+
+.table tbody td{
+    padding:6px 8px;
+    line-height:18px;
+    vertical-align:middle !important;
+    font-size:12px;
+}
+
+.table tbody tr:hover{
+    background:#f5f9ff;
+}
+
+.panel-heading{
+    background:#fff !important;
+    border-bottom:2px solid #2b8cff;
+    text-transform:uppercase;
+    letter-spacing:.5px;
+}
+
+.table-responsive{
+    border:1px solid #e6e6e6;
+    border-radius:8px;
+    overflow:hidden;
+}
+
+#search{
+    width:320px;
+    margin-bottom:12px;
+}
+
+.equal-btn{
+    width:32px;
+    height:28px;
+    padding:0;
+    margin:1px;
+    line-height:28px;
+}
+
+.badge-state{
+    background:#eaf6ea;
+    color:#228b22;
+    padding:2px 7px;
+    border-radius:10px;
+    font-size:11px;
+    font-weight:600;
+}
+
+.address-cell{
+    min-width:260px;
+    max-width:260px;
+    white-space:normal;
+    word-break:break-word;
+    line-height:18px;
+}
+
+.org-table{
+    overflow-x:auto;
+    overflow-y:hidden;
+    white-space:nowrap;
+}
+
+.org-table table{
+    min-width:1250px;
+}
+
+.search-box{
+    position:relative;
+    width:320px;
+    margin-bottom:12px;
+}
+
+.search-box i{
+    position:absolute;
+    left:12px;
+    top:9px;
+    color:#888;
+    z-index:2;
+}
+
+.search-box input{
+    padding-left:35px;
+    height:34px !important;
+}
+
+.org-title{
+    text-align:center;
+    background:#ffffff !important;
+    border-bottom:2px solid #2b8cff;
+    font-size:18px;
+    font-weight:700;
+    letter-spacing:1px;
+    color:#1d3557;
+    padding:12px 0;
+}
+
+.org-title i{
+    color:#2b8cff;
+    margin-right:8px;
+    font-size:16px;
+}
 </style>
 
 <div class="row">
@@ -135,9 +296,14 @@ include_once('layouts/header.php');
 <div class="row">
 
 <!-- ADD FORM -->
-<div class="col-md-3 add-box">
+<div class="col-md-12">
+
 <div class="panel panel-default">
-<div class="panel-heading">Add Organization</div>
+
+<div class="panel-heading text-center org-title">
+    <i class="glyphicon glyphicon-briefcase"></i>
+    ORGANIZATION
+</div>
 
 <div class="panel-body">
 
@@ -147,103 +313,208 @@ include_once('layouts/header.php');
 <input type="hidden" name="id" value="<?php echo $edit['id']; ?>">
 <?php } ?>
 
-<input type="text" maxlength="5" id="mnemonic" name="mnemonic" class="form-control"
-value="<?php echo $edit ? $edit['mnemonic'] : ''; ?>" placeholder="Mnemonic *" required><br>
-
 <?php
 $master_org = find_by_sql("
-SELECT org_name 
-FROM master_inventory.master_organization 
+SELECT org_name
+FROM master_inventory.master_organization
 WHERE org_id = '{$_SESSION['org_id']}'
 LIMIT 1
 ");
-
 $org_name = $master_org ? $master_org[0]['org_name'] : '';
 ?>
-<input type="text" class="form-control"
-value="<?php echo $edit ? $edit['org_name'] : $org_name; ?>" readonly>
 
-<input type="text" name="phone" class="form-control"
-value="<?php echo $edit ? $edit['phone'] : ''; ?>" placeholder="Phone No"><br>
+<div class="row">
 
-<input type="email" name="email" class="form-control"
-value="<?php echo $edit ? $edit['email'] : ''; ?>" placeholder="Email ID"><br>
+<div class="col-md-2">
+<label>Mnemonic</label>
+<input type="text" maxlength="5"
+name="mnemonic"
+class="form-control"
+value="<?php echo $edit ? $edit['mnemonic'] : ''; ?>"
+required>
+</div>
 
-<input type="text" name="contact" class="form-control"
-value="<?php echo $edit ? $edit['contact_person'] : ''; ?>" placeholder="Contact Person"><br>
+<div class="col-md-3">
+<label>Organization</label>
+<input type="text"
+class="form-control"
+value="<?php echo $edit ? $edit['org_name'] : $org_name; ?>"
+readonly>
+</div>
 
-<textarea name="address" class="form-control" placeholder="Address"><?php echo $edit ? $edit['address'] : ''; ?></textarea><br>
+<div class="col-md-2">
+<label>Phone</label>
+<input type="text"
+name="phone"
+class="form-control"
+value="<?php echo $edit ? $edit['phone'] : ''; ?>">
+</div>
 
-<?php if($gst_enabled == "Yes"): ?>
+<div class="col-md-3">
+<label>Email</label>
+<input type="email"
+name="email"
+class="form-control"
+value="<?php echo $edit ? $edit['email'] : ''; ?>">
+</div>
 
-<select name="state_id" id="state_id" class="form-control" required>
+<div class="col-md-2" style="padding-top:21px">
+
+<?php if($edit){ ?>
+
+<button
+class="btn btn-success btn-block"
+name="update_org">
+Update
+</button>
+
+<?php }else{ ?>
+
+<button
+class="btn btn-success btn-block"
+name="add_org">
+Save Organization
+</button>
+
+<?php } ?>
+
+</div>
+
+</div>
+
+<div class="row" style="margin-top:8px;">
+
+<div class="col-md-3">
+<label>Contact Person</label>
+<input
+type="text"
+name="contact"
+class="form-control"
+value="<?php echo $edit ? $edit['contact_person'] : ''; ?>">
+</div>
+
+<div class="col-md-3">
+<label>State</label>
+
+<select
+name="state_id"
+id="state_id"
+class="form-control">
+
 <option value="">Select State</option>
 
 <?php foreach($states as $s){ ?>
-<option value="<?php echo $s['id']; ?>"
-        data-code="<?php echo $s['state_code']; ?>"
-        <?php if($edit && $edit['state_id']==$s['id']) echo "selected"; ?>>
-  <?php echo $s['state_name']; ?>
+
+<option
+value="<?php echo $s['id'];?>"
+data-code="<?php echo $s['state_code'];?>"
+<?php if($edit && $edit['state_id']==$s['id']) echo "selected"; ?>>
+
+<?php echo $s['state_name'];?>
+
 </option>
+
 <?php } ?>
 
-</select><br>
+</select>
 
-<input type="text" name="state_code" id="state_code" class="form-control"
-value="<?php echo $edit ? $edit['state_code'] : ''; ?>" placeholder="State Code" readonly><br>
-<?php endif; ?>
+</div>
 
-<?php if($gst_enabled == "Yes"): ?>
+<div class="col-md-2">
 
-<input type="text"
-name="gst"
-id="gst"
+<label>State Code</label>
+
+<input
+type="text"
+id="state_code"
+name="state_code"
 class="form-control"
+readonly
+value="<?php echo $edit ? $edit['state_code'] : ''; ?>">
 
-value="<?php echo $edit ? $edit['gst_no'] : ''; ?>"
+</div>
 
-placeholder="Enter GST Number">
+<div class="col-md-4">
 
-<br>
+<label>GST No</label>
 
-<?php endif; ?>
+<input
+type="text"
+name="gst"
+class="form-control"
+value="<?php echo $edit ? $edit['gst_no'] : ''; ?>">
 
-<?php if($edit){ ?>
-<button name="update_org" class="btn btn-danger btn-block">Update</button>
-<a href="organization_master.php" class="btn btn-secondary btn-block">Cancel</a>
-<?php } else { ?>
-<button name="add_org" class="btn btn-danger btn-block">Save Organization</button>
-<button type="button" class="btn btn-secondary btn-block" onclick="document.getElementById('orgForm').reset();">Clear</button>
+</div>
+
+</div>
+
+<div class="row" style="margin-top:8px;">
+
+<div class="col-md-12">
+
+<label>Address</label>
+
+<textarea
+name="address"
+rows="1"
+class="form-control"><?php echo $edit ? $edit['address'] : ''; ?></textarea>
+
+</div>
+
+</div>
+
+<?php if(!$edit){ ?>
+
+<div class="text-right" style="margin-top:15px">
+
+<button
+type="button"
+class="btn btn-default"
+onclick="document.getElementById('orgForm').reset()">
+Clear
+</button>
+
+</div>
+
 <?php } ?>
 
 </form>
 
 </div>
 </div>
+
 </div>
 
 <!-- LIST -->
-<div class="col-md-9">
+<div class="col-md-12">
 <div class="panel panel-default">
 <div class="panel-heading">Organization List</div>
 
 <div class="panel-body">
 
-<input type="text" id="search" class="form-control" placeholder="Search organization..."><br>
+<div class="search-box">
+    <i class="glyphicon glyphicon-search"></i>
 
-<div class="table-responsive">
+    <input
+        type="text"
+        id="search"
+        class="form-control"
+        placeholder="Search organization...">
+</div>
+
+<div class="table-responsive org-table">
 
 <table class="table table-bordered table-striped">
 
 <thead>
 <tr>
-<th>#</th>
+<th width="45">#</th>
 <th>Mnemonic</th>
 <th>Organization</th>
 <th>Phone</th>
 <th>Email</th>
 <th>Contact</th>
-<th>Address</th>
+<th style="min-width:260px;">Address</th>
 <?php if($gst_enabled == "Yes"): ?>
 
 <th>State</th>
@@ -251,7 +522,7 @@ placeholder="Enter GST Number">
 <th>GST No</th>
 
 <?php endif; ?>
-<th>Action</th>
+<th width="90" class="text-center">Action</th>
 </tr>
 </thead>
 
@@ -265,22 +536,33 @@ placeholder="Enter GST Number">
 <td><?php echo $o['phone']; ?></td>
 <td><?php echo $o['email']; ?></td>
 <td><?php echo $o['contact_person']; ?></td>
-<td><?php echo $o['address']; ?></td>
+<td class="address-cell">
+<?php echo $o['address']; ?>
+</td>
 <?php if($gst_enabled == "Yes"): ?>
 
-<td><?php echo $o['state_name']; ?></td>
+<td>
+<span class="badge-state">
+<?php echo $o['state_name']; ?>
+</span>
+</td>
 
 <td><?php echo $o['state_code']; ?></td>
 
 <td><?php echo $o['gst_no']; ?></td>
 
 <?php endif; ?>
-<td>
-<a href="organization_master.php?edit=<?php echo $o['id']; ?>" class="btn btn-info btn-xs equal-btn">Edit</a>
+<td class="text-center">
+<a href="organization_master.php?edit=<?php echo $o['id']; ?>"
+class="btn btn-primary btn-xs equal-btn">
+<i class="glyphicon glyphicon-pencil"></i>
+</a>
 
-<a onclick="return confirm('Delete karna sure?')" 
-   href="organization_master.php?del=<?php echo $o['id']; ?>" 
-   class="btn btn-danger btn-xs equal-btn">Delete</a>
+<a onclick="return confirm('Delete karna sure?')"
+href="organization_master.php?del=<?php echo $o['id']; ?>"
+class="btn btn-danger btn-xs equal-btn">
+<i class="glyphicon glyphicon-trash"></i>
+</a>
 </td>
 </tr>
 <?php endforeach; ?>
