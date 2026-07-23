c4dbf4036c0f4b233e920c0d1a361a32f0ed3c46
anib9871 <anib9871@gmail.com>
2026-07-23 18:20:09 +0530

    Update invoice links in admin menu

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -91,8 +91,8 @@ if(!empty($config)){
         <li><a href="manage_grn.php">GRN</a></li>
         <!-- <li><a href="create_quotation.php">Quotation</a></li> -->
         <li><a href="quotation_list.php">Quotation</a></li>
-        <li><a href="invoice_create.php?system=inventory">Invoice</a></li>
-        <li><a href="invoice_list.php">Edit/Print Invoice</a></li>
+        <!-- <li><a href="invoice_create.php?system=inventory">Invoice</a></li> -->
+        <li><a href="invoice_list.php">Invoice</a></li>
         <li><a href="manufacture.php">Manufacture</a></li>
         <li><a href="return_master.php">Return</a></li>
 
