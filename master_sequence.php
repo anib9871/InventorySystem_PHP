488b28b2c00926eeba4a7555ada8e64668349c05
anib9871 <anib9871@gmail.com>
2026-07-31 12:08:08 +0530

    Update master_sequence.php

--- master_sequence.php
+++ master_sequence.php
@@ -409,13 +409,13 @@ document.getElementById("seqSearch").addEventListener("keyup", function() {
 // Delete Confirmation
 function confirmDelete(id) {
     Swal.fire({
-        title: 'Kya aap sure hain?',
-        text: "Is sequence ko delete kar diya jayega!",
+        title: 'Are You Sure?',
+        text: "This sequence will be deleted!",
         icon: 'warning',
         showCancelButton: true,
         confirmColor: '#ef4444',
         cancelColor: '#6b7280',
-        confirmButtonText: 'Haan, Delete Karo!',
+        confirmButtonText: 'Yes, Delete It!',
         cancelButtonText: 'Cancel'
     }).then((result) => {
         if (result.isConfirmed) {
