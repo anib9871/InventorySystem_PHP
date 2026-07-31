10f21d0fc15509880e34c81266f887bbad5c14b9
anib9871 <anib9871@gmail.com>
2026-07-31 12:01:28 +0530

    Update gst_master.php

--- gst_master.php
+++ gst_master.php
@@ -441,13 +441,13 @@ document.getElementById("search").addEventListener("keyup", function(){
 /* Delete confirmation */
 function confirmDelete(id) {
     Swal.fire({
-        title: 'Kya aap sure hain?',
-        text: "Is GST record ko delete kar diya jayega!",
+        title: 'Are You Sure?',
+        text: "This GST record will be deleted!",
         icon: 'warning',
         showCancelButton: true,
         confirmColor: '#ef4444',
         cancelColor: '#6b7280',
-        confirmButtonText: 'Haan, Delete Karo!',
+        confirmButtonText: 'Yes, Delete It!',
         cancelButtonText: 'Cancel'
     }).then((result) => {
         if (result.isConfirmed) {
