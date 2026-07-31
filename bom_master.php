ee5d60cd7580e238e9dcd28cc12e0e7478ab908a
anib9871 <anib9871@gmail.com>
2026-07-31 11:48:59 +0530

    Update bom_master.php

--- bom_master.php
+++ bom_master.php
@@ -663,13 +663,13 @@ document.addEventListener("click", function(e){
 /* Delete confirmation modal */
 function confirmDelete(id) {
     Swal.fire({
-        title: 'Kya aap sure hain?',
-        text: "Is pure BOM configuration ko delete kar diya jayega!",
+        title: 'Are You Sure?',
+        text: "These full BOM configration will be deleted.!",
         icon: 'warning',
         showCancelButton: true,
         confirmColor: '#ef4444',
         cancelColor: '#6b7280',
-        confirmButtonText: 'Haan, Delete Karo!',
+        confirmButtonText: 'Yes, Delete It!',
         cancelButtonText: 'Cancel'
     }).then((result) => {
         if (result.isConfirmed) {
